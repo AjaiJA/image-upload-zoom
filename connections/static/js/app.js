bf8b19a091a045129a842b9316ba764290f49eed
AjaiJA <ajaija2000@gmail.com>
2022-08-05 11:48:29 +0530

    Image Zoom Updated

--- connections/static/js/app.js
+++ connections/static/js/app.js
@@ -1,7 +1,13 @@
 let clickedPopupImg=document.querySelector('.popup-click-img')
 let totalImg=document.querySelectorAll('.image-view-zoom')
 
+for(let i=0;i<totalImg.length;i++){
+    totalImg[i].addEventListener('click',ApplyPopup)
+}
 
-// function (){
-
-// }
+function ApplyPopup(e){
+    let filePath=e.target.src
+    clickedPopupImg.src=filePath;
+    clickedPopupImg.alt=filePath
+    document.querySelector('.modal-img-path').innerHTML=filePath.replace("http://localhost:8000/Media/","");
+}
